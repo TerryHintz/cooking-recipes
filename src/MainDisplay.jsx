6d3dba177a067e456f87e6dbe0e1d1219b86360e
TerryHintz <cza82@sfu.ca>
2020-07-09 19:16:52 -0700

    sidenav set up

--- src/MainDisplay.jsx
+++ src/MainDisplay.jsx
@@ -1,8 +1,25 @@
 import React, {Component} from 'react';
 import Paper from '@material-ui/core/Paper';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
 import './Recipes.css';
 
 const default_recipe = [
+    {
+        title: 'Shrimp Fried Rice',
+        image: 'https://www.simplyrecipes.com/wp-content/uploads/2008/08/shrimp-fried-rice-horiz-a-1600.jpg',
+        desc: 'Do you ever have leftover rice in the fridge? A great way to use it up is to make fried rice! Fried rice works best with chilled leftover rice actually. Fresh rice can fry up a bit mushy. But leftover rice that had a chance to dry out a bit? Perfect for frying.',
+        ingredients: [
+            '8 ounces of shrimp',
+            '1/2 teaspoon of salt',
+            'freshly ground black pepper',
+        ],
+        steps: [
+            'Toss shrimp with salt, pepper, cornstarch',
+            'Heat pan on high heat',
+            'Sear shrimp on both sides',
+        ],
+    },
     {
         title: 'Shrimp Fried Rice',
         image: 'https://www.simplyrecipes.com/wp-content/uploads/2008/08/shrimp-fried-rice-horiz-a-1600.jpg',
@@ -32,40 +49,45 @@ class MainDisplay extends Component {
 
     render(){
         return(
-            <div>
-                {this.state.recipes.map((recipe) => {
-                    return(
-                        <Paper key={recipe.title} elevation={10} className='recipe-paper'>
-                            <div className='recipe-header'>{recipe.title}</div>
-                            <img src={recipe.image} alt={recipe.title} className='recipe-img'/>
-                            <p>{recipe.desc}</p>
-                            <div className='recipe-sub-section'>
-                                <div className='recipe-sub-header'>Ingredients</div>
-                                <ul>
-                                    {recipe.ingredients.map((item) => {
-                                        return (
-                                            <li key={item}>
-                                                {item}
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            <div className='recipe-sub-section'>
-                                <div className='recipe-sub-header'>Steps</div>
-                                <ol>
-                                    {recipe.steps.map((step) => {
-                                        return (
-                                            <li key={step}>
-                                                {step}
-                                            </li>
-                                        )
-                                    })}
-                                </ol>
-                            </div>
-                        </Paper>
-                    )
-                })}
+            <div className='recipe-root'>
+                <div className='recipe-drawer'>
+
+                </div>
+                <div className='recipe-container'>
+                    {this.state.recipes.map((recipe) => {
+                        return(
+                            <Paper key={recipe.title} elevation={10} className='recipe-paper'>
+                                <div className='recipe-header'>{recipe.title}</div>
+                                <img src={recipe.image} alt={recipe.title} className='recipe-img'/>
+                                <p>{recipe.desc}</p>
+                                <div className='recipe-sub-section'>
+                                    <div className='recipe-sub-header'>Ingredients</div>
+                                    <ul>
+                                        {recipe.ingredients.map((item) => {
+                                            return (
+                                                <li key={item}>
+                                                    {item}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                                <div className='recipe-sub-section'>
+                                    <div className='recipe-sub-header'>Steps</div>
+                                    <ol>
+                                        {recipe.steps.map((step) => {
+                                            return (
+                                                <li key={step}>
+                                                    {step}
+                                                </li>
+                                            )
+                                        })}
+                                    </ol>
+                                </div>
+                            </Paper>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 import MainDisplay from './MainDisplay';
-import LeftDrawer from './LeftDrawer';
 
 class Recipes extends Component {
     render(){
         return(
             <div>
-                <LeftDrawer/>
                 <MainDisplay/>
             </div>
         )
